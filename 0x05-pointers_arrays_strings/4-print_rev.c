ef38be958abef5ffd33422302af80985810030eb
Ghoufraneennili <ghoufraneen@gmail.com>
2023-03-28 09:36:42 -0700

    a

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@ void print_rev(char *s)
 {
 	int c = 0;
 
-	while (*s != '\0')
+	while (s[c] != '\0')
 	{
 		c++;
 	}
 
 	for (c -= 1 ; c >= 0 ; c--)
 	{
-		_putchar(*s);
+		_putchar(s[c]);
 	}
 
 	_putchar('\n');
