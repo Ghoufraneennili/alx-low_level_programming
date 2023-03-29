44113ab67be1a904c988f7092d6fa8d0b97a2f4f
Ghoufraneennili <ghoufraneen@gmail.com>
2023-03-28 17:56:46 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,8 +6,9 @@
  * Return: void
  */
 
-void rev_string(char *s);
+void rev_string(char *s)
 {
+
 	int len = 0, index = 0;
 	char tmp;
 
@@ -16,7 +17,7 @@ void rev_string(char *s);
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
-		tem = s[index];
+		tmp = s[index];
 		s[index] = s[len - index - 1];
 		s[len - index - 1] = tmp;
 	}
